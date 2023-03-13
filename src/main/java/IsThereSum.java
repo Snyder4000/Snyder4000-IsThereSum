ec694e8b10ab329c17776eb2218c2d3733dbd6de
Mark Snyder <Snyder4000@yahoo.com>
2023-03-13 12:12:17 +0000

    Completed Lab IsThereSum

--- src/main/java/IsThereSum.java
+++ src/main/java/IsThereSum.java
@@ -10,6 +10,16 @@ public class IsThereSum {
      * @return true if two separate values in the array add up to a target, false otherwise.
      */
     public boolean check(int[] arr, int target){
-        return false;
+        boolean bool = false;
+        for(int i = 0; i < arr.length - 1; i++){
+            if(arr[i]+arr[i+1]  == target){
+                bool = true;
+                break;
+            }
+            else{
+                bool = false;
+            }
+        }
+        return bool;
     }
 }
